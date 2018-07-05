a063627f06aff0cdf696122eb6ec26bccf3bb84e
Mahdi <mahdi@localhost.localdomain>
2018-07-06 01:06:51 +0430

    complete course 10

---
+++ public/static/js/app.js
@@ -0,0 +1,7 @@
+if ('serviceWorker' in navigator) {
+    navigator
+        .serviceWorker
+        .register('/service-worker.js',).then(registration =>{
+            console.log('Your service worker has been registred !',registration);
+        });
+}
